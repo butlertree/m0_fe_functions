44ece4a64bd525d28ad2a58451d04c9e2a28df14
Chris Butler <butlertree@gmail.com>
2023-08-03 22:16:44 -0600

    Changes to existing code

--- cleanUp.js
+++ cleanUp.js
@@ -7,38 +7,48 @@
     // using `node cleanUp.js` or copy and paste into a replit
 
 
-// EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+// EX 1: I put a return in and took the console.log outside of the code blocks. 
+function askForName() {
+  return "Hello, what is your name?";
+}
+  console.log (askForName())
 
-askForName()
 
-
-// EX 2:
+// EX 2: I put the addThreeNums(1, 2, 3) and (4, 2, 7) into the console.log statement. 
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
-
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
-
-
-// EX 3:
-func makeFreshPesto(){
-  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-  console.log("Pulse basil and pine nuts");
-  console.log("Add garlic and cheeses");
-  console.log("Slowly pour in oil");
-  console.log("Season");    }
-
-makeFreshPesto();
+  var sum = first + second + third;
+  return sum;
+        }
+  console.log(addThreeNums(1, 2, 3));
+  console.log(addThreeNums(4, 2, 7));
 
 
-//  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
-
-return avg
+// EX 3: There was nothing to return So I added a return and moved the function makeFreshPesto into the first console.log line.  
+function makeFreshPesto(){
+  return "Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper";
   }
+console.log(makeFreshPesto() + "Pluse basil and pine nuts" );
+console.log("Add garlic and cheeses");
+console.log("Slowly pour in oil");
+console.log("Season");
+
+
+
+
+
+//  EX 4:I added text to the return line.  I also added a variable as a stand in for the num1 and num2 and I added a console.log statement.  
+
+
+function average(num1, num2){
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  return `The average is ${avg}!`;
+ }
+ 
+ find1 = average(12, 35)
+ 
+ console.log(find1);
+
+
+
+
